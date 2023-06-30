4ef803999e619f8d9c89ea06823e6bd8f2c2b1f6
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-06-30 16:35:06 +0200

    UserRepository Update

--- Projekt_DatabaseApp/Repositories/UserRepository.cs
+++ Projekt_DatabaseApp/Repositories/UserRepository.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using Projekt_DatabaseApp.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projekt_DatabaseApp.Repositories
 {
-    internal class UserRepository
+    public class UserRepository : RepositoryBase, IUserRepository
     {
+        public void Add(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthenticateUser(NetworkCredential credential)
+        {
+            bool validUser;
+            using (var connection = GetConnection())
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "select *from [User] where username=@username and [password]=@password";
+                command.Parameters.Add("@username", SqlDbType.NVarChar).Value = credential.UserName;
+                command.Parameters.Add("@password", SqlDbType.NVarChar).Value = credential.Password;
+                validUser = command.ExecuteScalar() == null ? false : true;
+            }
+            return validUser;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetByAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UserModel userModel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
