11333f1928d9a4b6d587a213126426a58fa86c61
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-06-30 17:52:14 +0200

    App.xaml.cs Update + Creating UserAccountModel.cs
    "Log in" is working

--- Projekt_DatabaseApp/App.xaml.cs
+++ Projekt_DatabaseApp/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Projekt_DatabaseApp.View;
 
 namespace Projekt_DatabaseApp
 {
@@ -13,5 +14,19 @@ namespace Projekt_DatabaseApp
     /// </summary>
     public partial class App : Application
     {
+        protected void ApplicationStart(object sender, EventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainWindow(); // !!
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
     }
 }
