045c73090b9a6949da8e9a354228225210573049
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-06-30 18:19:43 +0200

    MainViewModel.cs Update

--- Projekt_DatabaseApp/Model/UserAccountModel.cs
+++ Projekt_DatabaseApp/Model/UserAccountModel.cs
@@ -8,5 +8,8 @@ namespace Projekt_DatabaseApp.Model
 {
     public class UserAccountModel
     {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public byte[] ProfilePicture { get; set; }
     }
 }
---
+++ Projekt_DatabaseApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Projekt_DatabaseApp.Model;
+using Projekt_DatabaseApp.Repositories;
+
+namespace Projekt_DatabaseApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private UserAccountModel _currentUserAccount;
+        private IUserRepository userRepository;
+
+        public UserAccountModel CurrentUserAccount
+        {
+            get { return _currentUserAccount; } 
+            set { _currentUserAccount = value; OnPropwrtyChanged(nameof(CurrentUserAccount)); }
+        }
+
+        public MainViewModel()
+        {
+            userRepository = new UserRepository();
+            LoadCurrentUserData();
+        }
+
+        private void LoadCurrentUserData()
+        {
+            var user = userRepository.GetByUsername(Thread.CurrentPrincipal.Identity.Name);
+            if (user != null)
+            {
+                CurrentUserAccount = new UserAccountModel()
+                {
+                    Username = user.Username,
+                    DisplayName = $"Welcome {user.Name} {user.LastName}",
+                    ProfilePicture = null
+                };
+            }
+            else
+            {
+                MessageBox.Show("Invalid user, not logged in");
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
