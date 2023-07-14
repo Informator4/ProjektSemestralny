d9add2517c3b02833c3c599210998a6449b09e1c
Mateusz M <61331751+Informator4@users.noreply.github.com>
2023-07-14 14:16:00 +0200

    Main Window Update 8
    + Dopracowano zakładki (dodano przyciski Add, Delete i Update);
    + Rozpoczynanie prac nad bazą danych.

--- Projekt_DatabaseApp/View/StarsView.xaml.cs
+++ Projekt_DatabaseApp/View/StarsView.xaml.cs
@@ -18,11 +18,92 @@ namespace Projekt_DatabaseApp.View
     /// <summary>
     /// Logika interakcji dla klasy StarsView.xaml
     /// </summary>
-    public partial class StarsView : UserControl
+    public partial class StarsView : UserControl  /*, IStarView*/
     {
         public StarsView()
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        //// ### Properties ###
+        //public string StarId 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarName 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarKind 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarDistance 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarNOP 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarDiameter 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string StarMass 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string Search 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public bool IsEdit 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public bool IsSuccessful 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+        //public string Message 
+        //{ 
+        //    get => throw new NotImplementedException(); 
+        //    set => throw new NotImplementedException(); 
+        //}
+
+        //// ### Events ###
+        //public event EventHandler SearchEvent;
+        //public event EventHandler AddEvent;
+        //public event EventHandler EditEvent;
+        //public event EventHandler DeleteEvent;
+        //public event EventHandler SaveEvent;
+        //public event EventHandler CancelEvent;
+
+        //// ### Methods ###
+        //public void SetStarListBindingSource(System.Windows.Forms.BindingSource netList)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Show()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
